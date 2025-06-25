bef931dee69e5947ce7250f963364f3cf90678cd
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2025-06-25 14:19:58 +0300

    Add testing, improve Makefile
    Signed-off-by: Georgios Ntoutsos <gntouts@nubificus.co.uk>

--- main.go
+++ main.go
@@ -8,15 +8,37 @@ import (
 
 var version string
 
-func main() {
-	args := os.Args[1:]
-
+// processArgs handles the logic for processing command line arguments
+func processArgs(args []string) (output string, shouldExit bool, exitCode int) {
 	// Handle --version flag
 	if len(args) == 1 && args[0] == "--version" {
-		fmt.Println(version)
-		return
+		return version, true, 0
+	}
+
+	// Handle empty args
+	if len(args) == 0 {
+		return "", false, 0
 	}
 
 	// Echo all arguments
-	fmt.Println(strings.Join(args, " "))
+	return strings.Join(args, " "), false, 0
+}
+
+// echoOutput handles the actual output printing
+func echoOutput(output string) {
+	fmt.Println(output)
+}
+
+func main() {
+	args := os.Args[1:]
+
+	output, shouldExit, exitCode := processArgs(args)
+
+	if shouldExit {
+		echoOutput(output)
+		os.Exit(exitCode)
+		return
+	}
+
+	echoOutput(output)
 }
